2679b01d4e92b8a3045ea3f048e23c5d0ed9ec69
Your Name <you@example.com>
2023-11-26 15:21:32 +0200

    add timer

--- dist/js/script.js
+++ dist/js/script.js
@@ -460,6 +460,42 @@ let _slideToggle = (target, duration = 500) => {
 }
 myPopup(); // ПОПАПЫ */
 
+class Timer {
+    constructor(data) {
+        this.body = document.querySelector(data.body);
+        this.minutes = +this.body.querySelector("[data-timer-minutes]").dataset.timerMinutes
+        this.seconds = +this.body.querySelector("[data-timer-seconds]").dataset.timerSeconds
+        this.totalSeconds = (this.minutes * 60) + this.seconds
+        this.start()
+    }
+
+    start() {
+        let allSeconds = (this.minutes * 60) + this.seconds
+
+        const timer = setInterval(() => {
+            allSeconds--
+            this.minutes = Math.floor(allSeconds / 60);
+            this.seconds = allSeconds - (Math.floor(allSeconds / 60) * 60)
+
+
+            this.body.querySelector("[data-timer-minutes]").textContent = String(this.minutes).length == 2 ? this.minutes : "0" + String(this.minutes);
+            this.body.querySelector("[data-timer-seconds]").textContent = String(this.seconds).length == 2 ? this.seconds : "0" + String(this.seconds);
+
+            this.precent = (100 * (1 - (allSeconds / this.totalSeconds))).toFixed(0)
+
+            this.body.querySelector("[data-timer-precent-value]").textContent = this.precent + "%";
+            this.body.querySelector("[data-timer-precent]").style.width = this.precent + "%";
+
+            if (allSeconds == 0) {
+                this.body.querySelector("[data-timer-audio]").volume = 0.2
+                this.body.querySelector("[data-timer-audio]").play()
+
+                clearInterval(timer);
+            }
+        }, 1000)
+    }
+}; // ТАЙМЕР 
+
 //< " СКРИПТЫ " >=============================================================================================================>//
 
 let isMobile = {
@@ -478,3 +514,11 @@ if (isMobile.any()) {
 }
 
 //< " СКРИПТЫ " >=============================================================================================================>//
+const wow = new WOW({
+	mobile: false,
+	offset: 300,
+}).init();
+
+const timer = new Timer({
+	body: ".main__left"
+})
